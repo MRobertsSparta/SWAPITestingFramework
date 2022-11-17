efb97bd39909e8121c52fcf4ee7767ba7afe05ec
riya353 <riyashaji353@gmail.com>
2022-11-17 14:05:54 +0000

    added plural and singular dtos for Species endpoint

---
+++ src/test/java/com/sparta/framework/dto/SpeciesCollectionDTO.java
@@ -0,0 +1,35 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sparta.framework.dto.SpeciesDTO;
+
+import java.util.List;
+
+public class SpeciesCollectionDTO{
+
+	@JsonProperty("next")
+	private String next;
+
+	@JsonProperty("previous")
+	private Object previous;
+
+	@JsonProperty("count")
+	private int count;
+
+	@JsonProperty("results")
+	private List<SpeciesDTO> results;
+
+	public String getNext(){
+		return next;
+	}
+
+	public Object getPrevious(){
+		return previous;
+	}
+
+	public int getCount(){
+		return count;
+	}
+
+	public List<SpeciesDTO> getResults(){
+		return results;
+	}
+}
---
+++ src/test/java/com/sparta/framework/dto/SpeciesDTO.java
@@ -0,0 +1,113 @@
+package com.sparta.framework.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class SpeciesDTO {
+
+	@JsonProperty("films")
+	private List<String> films;
+
+	@JsonProperty("skin_colors")
+	private String skinColors;
+
+	@JsonProperty("homeworld")
+	private String homeworld;
+
+	@JsonProperty("edited")
+	private String edited;
+
+	@JsonProperty("created")
+	private String created;
+
+	@JsonProperty("eye_colors")
+	private String eyeColors;
+
+	@JsonProperty("language")
+	private String language;
+
+	@JsonProperty("classification")
+	private String classification;
+
+	@JsonProperty("people")
+	private List<String> people;
+
+	@JsonProperty("url")
+	private String url;
+
+	@JsonProperty("hair_colors")
+	private String hairColors;
+
+	@JsonProperty("average_height")
+	private String averageHeight;
+
+	@JsonProperty("name")
+	private String name;
+
+	@JsonProperty("designation")
+	private String designation;
+
+	@JsonProperty("average_lifespan")
+	private String averageLifespan;
+
+	public List<String> getFilms(){
+		return films;
+	}
+
+	public String getSkinColors(){
+		return skinColors;
+	}
+
+	public String getHomeworld(){
+		return homeworld;
+	}
+
+	public String getEdited(){
+		return edited;
+	}
+
+	public String getCreated(){
+		return created;
+	}
+
+	public String getEyeColors(){
+		return eyeColors;
+	}
+
+	public String getLanguage(){
+		return language;
+	}
+
+	public String getClassification(){
+		return classification;
+	}
+
+	public List<String> getPeople(){
+		return people;
+	}
+
+	public String getUrl(){
+		return url;
+	}
+
+	public String getHairColors(){
+		return hairColors;
+	}
+
+	public String getAverageHeight(){
+		return averageHeight;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getDesignation(){
+		return designation;
+	}
+
+	public String getAverageLifespan(){
+		return averageLifespan;
+	}
+}
